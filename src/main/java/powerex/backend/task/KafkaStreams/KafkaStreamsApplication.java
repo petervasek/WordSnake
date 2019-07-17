e2749b4ad27778172992c8e9bf82d79235de94f0
vasekp <peter.vasek@softec.sk>
2019-07-17 12:24:02 +0200

    Initial commit

---
+++ src/main/java/powerex/backend/task/KafkaStreams/KafkaStreamsApplication.java
@@ -0,0 +1,13 @@
+package powerex.backend.task.KafkaStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/powerex/backend/task/KafkaStreams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,16 @@
+package powerex.backend.task.KafkaStreams;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaStreamsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
